68306370d49ade19ebffc58544188f17acb6d382
Juaquin Maria Serrano Macías <mithrillium@hotmail.com>
2020-11-30 21:41:12 +0100

    Estilo login ok y formulario básico ok.

---
+++ cambio.js
@@ -0,0 +1,72 @@
+var usuario = null;
+var nombre = "";
+window.onload = compruebaUsuario;
+//window.onload = iniciar;
+function compruebaUsuario() {
+    var encontrado = false;
+    var i = 0;
+    while (!encontrado) {
+        usuario = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        //Quizas tengo que comprobar si es un objeto
+        if (usuario.haEntrado) {
+            nombre = localStorage.key(i);
+            usuario.haEntrado = false;
+            localStorage.setItem(nombre, JSON.stringify(usuario));
+            encontrado = true;
+            //alert(nombre);
+            //document.getElementById("nom").innerHTML = nombre;
+        }
+        i++;
+    }
+    function calculaMovimientos() {
+        var mensaje = "";
+        var prueba = ""; //PRUEBA, BORRAR
+        usuario.acumuladoDesgaste = usuario.acumuladoDesgaste.sort((a, b) => b[1] - a[1]); //Este sort se hace solo una vez, al entregar el formulario de como se patina. No ene este docu.
+        usuario.valoresDeDesgaste = usuario.valoresDeDesgaste.sort((a, b) => a[1] - b[1]); //Este sort se hace cada vez que el usuario valla a cambiar ruedas.
+        for (var i = 0; i < 6; i++) {
+            var acumuladoPosicion = usuario.acumuladoDesgaste[i][2];
+            var valorPosicion = usuario.valoresDeDesgaste[i][2];
+            //alert("Rueda en posición " + acumuladoPosicion + " a posición " +valorPosicion + ".");   {{MENSAJE SALIDA}}
+            if(valorPosicion == acumuladoPosicion){
+                mensaje = mensaje + "Rueda en posición " + acumuladoPosicion + " sigue en la misma posición. Rotar la rueda.<br/>";
+            }else{
+                if(acumuladoPosicion >= 3){
+                    if(valorPosicion >= 3){
+                        mensaje = mensaje + "Rueda en posición" + acumuladoPosicion + " a posición "+ valorPosicion +" (mismo patín). Rotar la rueda.<br/>";
+                    }else{
+                        mensaje = mensaje + "Rueda en posición" + acumuladoPosicion + " a posición "+ valorPosicion +" (cambia de patín). Rotar la rueda.<br/>";
+                    }
+                }else{
+                    if(valorPosicion <= 2){
+                        mensaje = mensaje + "Rueda en posición" + acumuladoPosicion + " a posición "+ valorPosicion +" (mismo patín). Rotar la rueda.<br/>";
+                    }else{
+                        mensaje = mensaje + "Rueda en posición" + acumuladoPosicion + " a posición "+ valorPosicion +" (cambia de patín). Rotar la rueda.<br/>";
+                    }
+                }
+            }
+            prueba = prueba + usuario.acumuladoDesgaste[i][0] + " - "+ usuario.acumuladoDesgaste[i][1]+"<br/>"; //PRUEBA, BORRAR
+            //La rueda gira, giran los valores.
+            var auxiliar = usuario.acumuladoDesgaste[i][0];
+            usuario.acumuladoDesgaste[i][0] = usuario.acumuladoDesgaste[i][1];
+            usuario.acumuladoDesgaste[i][1] = auxiliar;
+            //Una vez acumulado el mensaje de esta rueda, su nueva posición es valorPosición.
+            usuario.acumuladoDesgaste[i][2] = valorPosicion;
+            //Sumo valores (acumulo) para la proxima vez que pulse el boton.
+            usuario.acumuladoDesgaste[i][0] = usuario.acumuladoDesgaste[i][0] + usuario.valoresDeDesgaste[i][0];
+            usuario.acumuladoDesgaste[i][1] = usuario.acumuladoDesgaste[i][1] + usuario.valoresDeDesgaste[i][1];
+            /*
+            En este for tambien hay que comprobar:
+                1. Si cambia o no de posición.
+                2. Si cambia, si cambia o no de patín.
+                3. Si gira sobre si mismo o no 
+                    3.1 Mismo patín [x][0] < [x][1] rota. Osea, si el exterior esta menos desgastado, rota. 
+                    3.2 Cambia patín [x][0] > [x][1] rota. Osea, si el exterior está más desgastado, será otra vez exterior.
+            */
+        }
+        //Sobreescribo el usuario con sus cambios.
+        localStorage.setItem(nombre, JSON.stringify(usuario));
+        document.getElementById("confirmacion").innerHTML = mensaje;
+        document.getElementById("prueba").innerHTML = prueba; //PRUEBA, BORRAR
+    }
+    document.getElementById("calcular").addEventListener('click', calculaMovimientos, false);
+}
---
+++ login.js
@@ -0,0 +1,51 @@
+window.onload = iniciar;
+
+function iniciar() {
+    //Variables globales a la funcion iniciar
+    var encontrado = false;
+    var nombre = "";
+
+    function compruebaNombre() {
+        //Busqueda secuencial del nombre en localStorage
+        var i = 0;
+        while (i < localStorage.length && this.value != localStorage.key(i)) {
+            i++;
+        }
+        if (i == localStorage.length) { //si no ha encontrado el nombre
+            document.getElementById("confirmacion").innerHTML = "No se encuentra el usuario.";
+            encontrado = false;
+        } else {
+            document.getElementById("confirmacion").innerHTML = "";
+            nombre = localStorage.key(i); //Aqui guardo el nombre, que es la key del localStorage
+            encontrado = true;
+        }
+    }
+    document.getElementById("nombre").addEventListener('blur', compruebaNombre, false);
+
+    function compruebaLogin() {
+        event.preventDefault();
+        if (encontrado) {
+            objetoNombre = JSON.parse(localStorage.getItem(nombre));
+            if (document.getElementById("pass").value != objetoNombre.pass) {
+                document.getElementById("confirmacion").innerHTML = "La contraseña no es correcta";
+                //alert( objetoNombre.contrasenia);
+            } else {
+                document.getElementById("confirmacion").innerHTML = "";
+                if (objetoNombre.haCompletadoFormulario) {
+                    objetoNombre.haEntrado = true;
+                    localStorage.setItem(nombre, JSON.stringify(objetoNombre)); //sobreescribe el mismo item, cambiando solo el haEncontrado
+                    //va a página de calcular cambios
+                    //Poner haEntrado a true.((IMPORTANTE))
+                    window.open("cambiaRuedas.html", "_self");
+                } else {
+                    objetoNombre.haEntrado = true;
+                    localStorage.setItem(nombre, JSON.stringify(objetoNombre));
+                    //va a página formulario de cómo patina
+                    //Poner haEntrado a true.((IMPORTANTE))
+                    window.open("formularioPrincipal.html", "_self");
+                }
+            }
+        }
+    }
+    document.getElementById("enviar").addEventListener('click', compruebaLogin, false);
+}
